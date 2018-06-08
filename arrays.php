f0af976eca4b8447427f2da081231e189a34ce09
evagolabek <golabek.eva@gmail.com>
2018-06-08 15:41:16 +0200

    arrays

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+#ARRAYS - variables that can hold multiple values
+/*
+Indexed
+Associative
+Multi-dimensional
+*/
+
+//indexed
+
+$people = array('Joe', 'Maria', 'Douglas');
+echo $people[1];
+
+$numbers = array(4, 7, 9);
+$cars = ['bmw', 'mazda', 'porshe'];
+$cars[2] = 'opel';
+$cars[] = 'jaguar';
+echo $cars[3];
+
+
+ ?>
