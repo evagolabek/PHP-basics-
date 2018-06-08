d50b46739d50540cc3f14f09764443ee1033ef95
evagolabek <golabek.eva@gmail.com>
2018-06-08 16:03:07 +0200

    arrays finished

--- arrays.php
+++ arrays.php
@@ -18,5 +18,31 @@ $cars[2] = 'opel';
 $cars[] = 'jaguar';
 echo $cars[3];
 
+echo count($cars);
+$print_r($cars); //prints out the entire array to the screen, handy for debugging
+var_dump($cars); //shows what it is, its value, key and value and data type and numbers of characters, tells you everything
+
+//associative
+
+$people = array('Brad' => 35, 'John' => 45);
+echo $people['Brad'];//you get 35
+
+$ids = [10 => 'Brad', 11 => 'John'];
+echo $ids[10]; //you get Brad
+
+$people['Jill'] = 44;
+echo $people['Jill']; //you get 44
+
+print_r($people); //shows array with key and its value, same as var_dump
+
+//multi-dimensional
+
+$cars = array(
+  array('BMW', 20, 10),
+  array('Mazda', 30, 15),
+  array('Porshe', 10, 3),
+); //e.g. number of cars total and number of cars sold
+
+  echo $cars[1][0]; //will give us details of number 1 of the array and index 0 which is Mazda
 
  ?>
