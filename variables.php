9051c64bafaf5aee8477775c5e1f2dbafa513546
evagolabek <golabek.eva@gmail.com>
2018-06-08 14:38:48 +0200

    first commit

---
+++ variables.php
@@ -0,0 +1,23 @@
+<?php
+echo "hello world";
+print "Hello World"; //same thing but echo is faster
+//single line comment
+#single line comment
+
+/*
+multiline
+comment
+*/
+
+#VARIABLES:
+/*
+prefix with the $
+starts with a letter or underscore
+only letters, numbers, underscore, case sensitive
+
+
+*/
+$output = "Hi there!";
+
+echo $output;
+?>
