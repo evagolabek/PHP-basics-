1cd217ab4e8febb68b8c8192a0e45bc4413c7e61
evagolabek <golabek.eva@gmail.com>
2018-06-09 23:27:30 +0200

    start website1

---
+++ website1/index.php
@@ -0,0 +1,5 @@
+<?php include 'inc/header.php'; ?>
+  <h1>Home</h1>
+<?php include 'inc/footer.php'; ?>
+
+//difference between include and require is that with include it will continue regardless anything ..
