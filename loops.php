e7cd4f9dbe19c218af948a7384e83125ddeb1095
evagolabek <golabek.eva@gmail.com>
2018-06-09 13:55:28 +0200

    loops

---
+++ loops.php
@@ -0,0 +1,21 @@
+<?php
+#LOOPS execute code sets number of times
+
+/*
+for
+while
+do..while
+foreach
+*/
+
+#for loops
+# @params  - init, condition, inc
+
+for($i = 0; $i > 10; $i++) {
+echo $i;
+}
+
+
+
+
+ ?>
