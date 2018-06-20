6bc2faa8092ca80cd49d31fecf3e79fadd4b76f3
evagolabek <golabek.eva@gmail.com>
2018-06-20 11:01:34 +0800

    php contact form

---
+++ website3/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Contact Us</title>
+  <link rel = "stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.min.css"
+</head>
+<body>
+  <nav class ="navbar-default">
+    <div class ="container">
+      <div class ="navbar-header">
+        <a class ="navbar-brand" href="index.php">My Website</a>
+      </div>
+    </div>
+  </nav>
+  <div class = "container">
+    <form method = "post" action ="">
+      <div class = "form-group">
+          <label>Name</label>
+          <input type = "text" name = "name" class = "form-control"
+          value ="">
+      </div>
+      <div class = "form-group">
+          <label>Email</label>
+          <input type = "text" name = "email" class = "form-control"
+          value ="">
+      </div>
+      <div class = "form-group">
+          <label>Message</label>
+          <textarea name = "message" class = "form-control"></textarea>
+      </div>
+      <br>
+      <button type = "submit" name ="submit" class ="btn btn-primary">
+      Sumbit</button>
+    </form>
+  </div>
+</body>
+</html>
