14cf1528697eaee0220825e9fe8694d68d05043f
evagolabek <golabek.eva@gmail.com>
2018-06-24 15:03:58 +0800

    php contact form fields validation

--- website3/index.php
+++ website3/index.php
@@ -1,3 +1,40 @@
+
+<?php
+//message vars
+$msg = '';
+$msgClass = '';
+
+//check for submit
+if(filter_has_var(INPUT_POST, 'submit')){
+  //echo 'Submitted';
+    //get he form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    //check required fields
+
+      if(!empty($email) && !empty($name) && !empty($message)) {
+      //passed
+      //check email
+      if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+        //failed
+        $msg = 'Please fill in all fields';
+        $msgClass = 'alert-danger';
+      } else {
+        //passed
+      }
+
+    } else {
+      //failed
+      $msg = 'Please fill in all fields';
+      $msgClass = 'alert-danger';
+      }
+}
+
+
+
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,20 +50,28 @@
     </div>
   </nav>
   <div class = "container">
-    <form method = "post" action ="">
+    <?php if($msg != '') : ?>
+      <div class = "alert <?php echo $msgClass; ?>"><?php echo $msg;
+      ?></div>
+
+    <?php endif; ?>
+    <form method = "post" action ="<?php echo $_SERVER['PHP_SELF']; ?>"
+      >
       <div class = "form-group">
           <label>Name</label>
           <input type = "text" name = "name" class = "form-control"
-          value ="">
+          value ="<?php echo isset($_POST['name']) ? $name : ''; ?>
+          ">
       </div>
       <div class = "form-group">
           <label>Email</label>
           <input type = "text" name = "email" class = "form-control"
-          value ="">
+          value ="<?php echo isset($_POST['email']) ? $email : ''; ?>">
       </div>
       <div class = "form-group">
           <label>Message</label>
-          <textarea name = "message" class = "form-control"></textarea>
+          <textarea name = "message" class = "form-control"><?php echo isset($_POST['message']) ? $message : ''; ?>
+          </textarea>
       </div>
       <br>
       <button type = "submit" name ="submit" class ="btn btn-primary">
