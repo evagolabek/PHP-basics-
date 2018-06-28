4a05286a32c34c5cb229b0794ce8b64963802a36
evagolabek <golabek.eva@gmail.com>
2018-06-28 17:41:04 +0800

    file system functions finish

--- website7/index.php
+++ website7/index.php
@@ -18,4 +18,55 @@
   //echo realpath($file);
 
   //checks to see if file
-  echo is_file($file);
+  //echo files_exists('test'); //it will show 1 whereis below it won't show
+  //echo is_file('test');
+
+  //check if file is writable
+  //echo is_writable($file);
+
+  //check if file is readable
+  //echo is_readable($file);
+
+  //get the file size
+  //echo filesize($file);
+
+//create a directory
+//mkdir('testing');
+
+//removes directory if empty
+//rmdir('testing');
+
+//copy file
+//echo copy('file1.txt', 'file2.txt');
+
+
+//rename files
+//rename('file2.txt', 'myfile.txt');
+
+//delete file
+//unlink('myfile.txt');
+
+//write from file to string
+//echo file_get_contents($file);
+
+//write a string to the file
+//echo file_put_contents($file, 'Goodbye World');
+
+//$current = file_get_contents($file);
+
+//$current .= ' Goodbye world';
+
+//file_put_contents($file, $current);
+
+//open file for reading
+//$handle = fopen($file,'r');
+//$data = fread($handle, filesize($file));
+//echo $data;
+
+//open file for writing
+// $handle = fopen('file2.txt', 'w');
+// $txt = "John Doe\n";
+// fwrite($handle, $txt);
+// $txt = "Steve Smith\n";
+// fwrite($handle, $txt);
+// fclose($handle);
