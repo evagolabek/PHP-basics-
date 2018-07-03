7c2cf595bb54352d9c354798387abd66e135979d
evagolabek <golabek.eva@gmail.com>
2018-07-03 12:13:13 +0800

    redirect from the list of posts to a single post

--- website8/config/db.php
+++ website8/config/db.php
@@ -6,5 +6,5 @@ $conn = mysqli_connect('localhost', 'root', '12345', 'phpblog');
 //check the connection
 if(mysqli_connect_errno()){
   //connection failed
-  echo'Failed to connect to MySQL'. mysqli_connect_errno;
+  echo'Failed to connect to MySQL'. mysqli_connect_errno();
 }
--- website8/post.php
+++ website8/post.php
@@ -3,14 +3,17 @@
 require('config/config.php');
 require('config/db.php');
 
+//get id
+$id = mysqli_real_escape_string($conn, $_GET['id']); //espace any dangerous characters
+
 //Create Query
-$query = 'SELECT * FROM posts';
+$query = 'SELECT * FROM posts WHERE id= '.$id;
 
 //Get Result
 $result = mysqli_query($conn, $query);
 
 //Fetch data
-$posts = mysqli_fetch_all($result, MYSQLI_ASSOC); //assoc of array: ['name' => 'Brand']
+$post = mysqli_fetch_assoc($result);
 //var_dump($posts); //prints the assoc of array
 
 //free the result
@@ -30,17 +33,11 @@ mysqli_close($conn);
     </head>
   <body>
     <div class="container">
-    <h1>Posts</h1>
-      <?php foreach ($posts as $post) : ?>
-        <div class="well">
-          <h3><?php echo $post['title']; ?></h3>
+          <a href="<?php echo ROOT_URL; ?>" class="btn btn-default">Back</a>
+          <h1><?php echo $post['title']; ?></h1>
           <small>Created on<?php echo $post['created_at']; ?>
           by <?php echo $post['author']; ?></small>
           <p><?php echo $post['body']; ?></p>
-          <a class ="btn btn-default" href="<?php echo ROOT_URL; ?>post.php?id=<?php
-            echo $post['id']; ?>">Read More</a>
-        </div>
-      <?php endforeach; ?>
-  </div>
+    </div>
   </body>
 </html>
