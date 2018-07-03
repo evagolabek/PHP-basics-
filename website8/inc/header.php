f824024f374321a088ba4d65f120533ff86449a1
evagolabek <golabek.eva@gmail.com>
2018-07-03 13:01:40 +0800

    navbar and change bootstrap address to 3

--- website8/inc/header.php
+++ website8/inc/header.php
@@ -2,6 +2,7 @@
   <html>
     <head>
       <title>PHP Blog</title>
-      <link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/cerulean/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="https://bootswatch.com/3/cerulean/bootstrap.min.css">
     </head>
   <body>
+  <?php include('navbar.php') ?>
