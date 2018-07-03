60b93f7f92dc52fd562a68b80cd1045d7b4ed599
evagolabek <golabek.eva@gmail.com>
2018-07-03 10:54:45 +0800

    bootstrap and layout

--- website8/index.php
+++ website8/index.php
@@ -10,9 +10,34 @@ $result = mysqli_query($conn, $query);
 
 //Fetch data
 $posts = mysqli_fetch_all($result, MYSQLI_ASSOC); //assoc of array: ['name' => 'Brand']
-var_dump($posts);
+//var_dump($posts); //prints the assoc of array
+
 //free the result
 mysqli_free_result($result);
 
 //close the connection
 mysqli_close($conn);
+
+?>
+
+
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>PHP Blog</title>
+      <link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/cerulean/bootstrap.min.css">
+    </head>
+  <body>
+    <div class="container">
+    <h1>Posts</h1>
+      <?php foreach ($posts as $post) : ?>
+        <div class="well">
+          <h3><?php echo $post['title']; ?></h3>
+          <small>Created on<?php echo $post['created_at']; ?>
+          by <?php echo $post['author']; ?></small>
+          <p><?php echo $post['body']; ?></p>
+        </div>
+      <?php endforeach; ?>
+  </div>
+  </body>
+</html>
