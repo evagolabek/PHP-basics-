86c80e7455581976676891ac720d0b23d14e130f
evagolabek <golabek.eva@gmail.com>
2018-07-04 11:57:52 +0800

    fix responsive navigation with js bootstrap file

--- website8/inc/footer.php
+++ website8/inc/footer.php
@@ -1,2 +1,9 @@
+<script
+  src="https://code.jquery.com/jquery-1.12.4.min.js"
+  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+  crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/
+bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
 </body>
 </html>
